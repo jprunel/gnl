3c69429ad241514e31b01b0f89d341854ae5c47f
jprunel <jprunel@users.noreply.github.com>
2019-02-13 18:51:02 +0100

    Update get_next_line.c

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: jprunel <jprunel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/15 17:19:58 by jprunel           #+#    #+#             */
-/*   Updated: 2019/02/13 16:37:33 by jprunel          ###   ########.fr       */
+/*   Updated: 2019/02/13 18:13:56 by jprunel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,49 +33,41 @@ int	get_next_line(const int fd, char **line)
  	int			status;
 
 	//
-			//ft_putendl(""); ft_putendl("\n**** GNL START ****");
+			ft_putendl(""); ft_putendl("\n**** GNL START ****");
 	//
-
 	if (!str && !(str = ft_strnew(BUFF_SIZE))) ////initialize str if !str
 		return (-1);
 	MALLCHECK((tmp = ft_strnew(BUFF_SIZE))); ////initialize tmp
 	//
-		//	ft_putstr("str start = "); ft_putendl(str);
+			ft_putstr("str start = "); ft_putendl(str);
 	//
-
 	if (str && (ft_with_eol(str))) ///// SI str contient un \n
 	{
 		*line = ft_strncpy(tmp, str, (ft_with_eol(str) - 1)); ////copie str jusqu'au \n dans tmp et l'assigne a line
 		str = ft_strcpy(str, str + ft_with_eol(str)); //recommence str apres le \n
 		//
-		//	ft_putstr("str end ="); ft_putendl(str);
+			ft_putstr("str end ="); ft_putendl(str);
 		//
 		return (1);
 	}
-	/*test
-	else if (str && !ft_with_eol(str) && ft_strlen(str) < BUFF_SIZE)
-	{
-		*line = ft_strcpy(tmp, str);
-		ft_strdel(&str);
-		return(1);
-	}
-	end test*/
 	if ((status = read(fd, tmp, BUFF_SIZE)) < 0)
 		return (-1);
 	//
-		 /*ft_putendl(""); ft_putstr("status = "); ft_putnbr(status); MENDATORY*/ // ft_putendl("");
+		ft_putendl(""); ft_putstr("status = "); ft_putnbr(status); ft_putendl("");
 	//
 	str = ft_strjoin(str, tmp);
-	//
-		//ft_putstr("ft_with_eol() res = "); ft_putnbr(ft_with_eol(str)); ft_putendl("");
-	//
-	if ((status < BUFF_SIZE) && (!ft_with_eol(str)))
+	if (str && str[0] && !ft_with_eol(str) && (status < BUFF_SIZE))
 	{
-		//ft_putendl("Get in last IF statment");
-		*line = str;
-		return (0);
+			*line = ft_strcpy(tmp, str);
+			ft_bzero(str, ft_strlen(str));
+			return (1);
 	}
 	ft_strdel(&tmp);
+	//
+		ft_putstr("ft_with_eol() res = "); ft_putnbr(ft_with_eol(str)); ft_putendl("");
+	//
+	if ((status < BUFF_SIZE) && !str[0])
+		return (0);
 	if (!get_next_line(fd, line))
 		return (0);
 	return (1);
